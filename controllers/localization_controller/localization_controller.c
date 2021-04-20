5a9b19df9aa90ef2509772e305cc1022f21775e1
jermayo <42137167+jermayo@users.noreply.github.com>
2021-04-20 16:25:01 +0200

    GPS working (normally)

--- controllers/localization_controller/localization_controller.c
+++ controllers/localization_controller/localization_controller.c
@@ -21,12 +21,36 @@ typedef struct
   double right_enc;
 } measurement_t;
 
+typedef struct 
+{
+  double x;
+  double y;
+  double heading;
+} pose_t;
+
+typedef struct
+{
+  pose_t pos;
+  pose_t speed;
+  int id;
+} robot_t;
+
 WbDeviceTag dev_gps;
 WbDeviceTag dev_acc;
 WbDeviceTag dev_left_encoder;
 WbDeviceTag dev_right_encoder;
 WbDeviceTag dev_left_motor; 
 WbDeviceTag dev_right_motor; 
+
+//-----------------------------------------------------------------------------------//
+/*VARIABLES*/
+static measurement_t  _meas;
+static robot_t        _robot;
+//static motor_t        _motor = {false, true, true, MAX_SPEED / 4.0};
+double last_gps_time_s = 0.0f;
+//static FILE *fp;
+
+//-----------------------------------------------------------------------------------//
 
 void init_devices(int ts);
 
@@ -51,6 +75,40 @@ void init_devices(int ts) {
   wb_motor_set_velocity(dev_right_motor, 0.0);
 }
 
+/**
+ * @brief     Get the gps measurements for the position of the robot. Get the heading angle. Fill the pose structure. 
+ */
+void controller_get_pose(){
+  double time_now_s = wb_robot_get_time();
+
+  memcpy(_meas.prev_gps, _meas.gps, sizeof(_meas.gps));
+  const double * gps_position = wb_gps_get_values(dev_gps);
+  memcpy(_meas.gps, gps_position, sizeof(_meas.gps));
+
+  //if(VERBOSE_GPS)
+  //  printf("ROBOT gps is at position: %g %g %g\n", _meas.gps[0], _meas.gps[1], _meas.gps[2]);
+  
+  _robot.pos.x = _meas.gps[0];
+  _robot.pos.y = _meas.gps[2];
+   /* 
+  if (time_now_s - last_gps_time_s > 0.1f) {
+    last_gps_time_s = time_now_s;
+    controller_get_gps();
+
+    // To Do : Fill the structure pose_t {x, y, heading}. Use the _pose_origin.
+    _pose.x = _meas.gps[0] - _pose_origin.x;
+    
+    _pose.y = -(_meas.gps[2] - _pose_origin.y);
+    
+    _pose.heading = controller_get_heading() + _pose_origin.heading;
+  
+    if(VERBOSE_POSE)
+      printf("ROBOT pose : %g %g %g\n", _pose.x , _pose.y , RAD2DEG(_pose.heading));
+  }*/
+  
+}
+
+
 int main() 
 {
   wb_robot_init();
@@ -60,10 +118,14 @@ int main()
   while (wb_robot_step(time_step) != -1)  {
   // Use one of the two trajectories.
     trajectory_1(dev_left_motor, dev_right_motor);
+    
+    
+    controller_get_pose();
+    
+    printf("ROBOT pose : %g %g\n", _robot.pos.x , _robot.pos.y);
 //    trajectory_2(dev_left_motor, dev_right_motor);
   
   }
   
-  
-  
 }
+

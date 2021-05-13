9825d73385dbd9fd1fd2d6e6746766f221bf6ebb
jermayo <42137167+jermayo@users.noreply.github.com>
2021-05-13 18:38:20 +0200

    Fixed Kalman

--- controllers/localization_controller/utils.h
+++ controllers/localization_controller/utils.h
@@ -39,8 +39,8 @@ void mult(double a[][MMS],double b[][MMS],double res[][MMS],int r1,int c1,int r2
     
     int i,j,k;
     /* Initializing elements of matrix mult to 0.*/
-    for(i=0; i<r2; ++i)
-        for(j=0; j<c1; ++j)
+    for(i=0; i<r1; ++i)
+        for(j=0; j<c2; ++j)
             res[i][j]=0;
         
     /* Multiplying matrix a and b and storing in array mult. */
@@ -49,13 +49,24 @@ void mult(double a[][MMS],double b[][MMS],double res[][MMS],int r1,int c1,int r2
             for(k=0; k<c1; ++k)
                 res[i][j]+=a[i][k]*b[k][j];         
 }
+
  
 void inv(double a[][MMS], double res[][MMS]){
-  double det=1/(a[0][0]*a[1][1]-a[0][1]*a[1][0]);
+  double det=1./((double) (a[0][0]*a[1][1]-a[0][1]*a[1][0]));
   res[0][0]=a[1][1]*det;
   res[1][1]=a[0][0]*det;
   res[1][0]=-a[1][0]*det;
   res[0][1]=-a[0][1]*det;
+  
+  int i,j;
+  for (i=0; i<2; i++){
+    for (j=0; j<2; j++){
+      if (fabs(res[i][j])<1E-7){
+        res[i][j]=0;
+      }
+    }
+  }
+  
 }
  
 void copy_matrix(double a[MMS][MMS], double res[MMS][MMS], int r1, int c1){
@@ -70,12 +81,14 @@ void copy_matrix(double a[MMS][MMS], double res[MMS][MMS], int r1, int c1){
 void print_matrix(double a[][MMS], int row, int col)
 {
     int i, j;
-    for(i=0; i<row; ++i)
+    printf("[");
+    for(i=0; i<row; ++i){
         for(j=0; j<col; ++j){
             printf("%g  ",a[i][j]);
-            if(j==col-1)
-                printf("\n\n");
         }
+        printf(";\n");
+    }
+    printf("]\n"); 
 }
 
 

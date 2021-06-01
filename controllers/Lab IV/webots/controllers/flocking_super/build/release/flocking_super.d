8200aef1cbafe3e38f027ff0d5f9ec7a8a28b7d4
MaximeZufferey <maxime.zufferey@epfl.ch>
2021-06-01 11:10:13 +0200

    Lab 4

---
+++ controllers/Lab IV/webots/controllers/flocking_super/build/release/flocking_super.d
@@ -0,0 +1,6 @@
+build/release/flocking_super.o: flocking_super.c \
+ C:\Users\maxim\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\maxim\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\maxim\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\maxim\AppData\Local\Programs\Webots/include/controller/c/webots/emitter.h \
+ C:\Users\maxim\AppData\Local\Programs\Webots/include/controller/c/webots/supervisor.h
